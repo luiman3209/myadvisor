4f7ea90f88001b692c528f97af8c346445eee7a9
Luigi <luigimangione320@gmail.com>
2024-06-22 17:57:52 +0200

    fixed some configs

--- resources/db_schema_definition.sql
+++ resources/db_schema_definition.sql
@@ -1,4 +1,3 @@
-
 -- user_configs table
 CREATE TABLE myadvisor.user_configs (
     user_id SERIAL PRIMARY KEY,
@@ -65,24 +64,24 @@ CREATE TABLE myadvisor.qualifications (
     abbreviation VARCHAR(10) NOT NULL
 );
 
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Certified Financial Planner', 'CFP');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Chartered Financial Analyst', 'CFA');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Certified Public Accountant', 'CPA');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Certified Investment Management Analyst', 'CIMA');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Certified Fund Specialist', 'CFS');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Certified Private Wealth Advisor', 'CPWA');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Chartered Life Underwriter', 'CLU');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Chartered Financial Consultant', 'ChFC');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Financial Risk Manager', 'FRM');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Accredited Asset Management Specialist', 'AAMS');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Series 6 License', 'Series 6');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Series 7 License', 'Series 7');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Series 65 License', 'Series 65');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Series 66 License', 'Series 66');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Certified Trust and Fiduciary Advisor', 'CTFA');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Accredited Investment Fiduciary', 'AIF');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Retirement Income Certified Professional', 'RICP');
-INSERT INTO qualifications (qualification_name, abbreviation) VALUES ('Certified Retirement Counselor', 'CRC');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Certified Financial Planner', 'CFP');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Chartered Financial Analyst', 'CFA');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Certified Public Accountant', 'CPA');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Certified Investment Management Analyst', 'CIMA');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Certified Fund Specialist', 'CFS');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Certified Private Wealth Advisor', 'CPWA');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Chartered Life Underwriter', 'CLU');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Chartered Financial Consultant', 'ChFC');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Financial Risk Manager', 'FRM');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Accredited Asset Management Specialist', 'AAMS');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Series 6 License', 'Series 6');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Series 7 License', 'Series 7');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Series 65 License', 'Series 65');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Series 66 License', 'Series 66');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Certified Trust and Fiduciary Advisor', 'CTFA');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Accredited Investment Fiduciary', 'AIF');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Retirement Income Certified Professional', 'RICP');
+INSERT INTO  myadvisor.qualifications (qualification_name, abbreviation) VALUES ('Certified Retirement Counselor', 'CRC');
 
 
 CREATE TABLE myadvisor.advisor_qualifications (
@@ -93,22 +92,7 @@ CREATE TABLE myadvisor.advisor_qualifications (
     FOREIGN KEY (qualification_id) REFERENCES myadvisor.qualifications(qualification_id) ON DELETE CASCADE
 );
 
---INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Retirement Planning', 'retirement_planning');
---INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Investment Management', 'investment_management');
---INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Tax Planning', 'tax_planning');
---INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Estate Planning', 'estate_planning');
---INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Insurance Planning', 'insurance_planning');
---INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Education Planning', 'education_planning');
---INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Debt Management', 'debt_management');
---INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Small Business Planning', 'small_business_planning');
---INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Divorce Planning', 'divorce_planning');
---INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Elder Care Planning', 'elder_care_planning');
---INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Charitable Giving And Philanthropy', 'charitable_giving_and_philanthropy');
---INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Behavioral Finance', 'behavioral_finance');
---INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Wealth Management', 'wealth_management');
---INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Risk Management', 'risk_management');
---INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Financial Education And Coaching', 'financial_education_and_coaching');
-                  
+              
 CREATE TABLE myadvisor.service_types (
     service_id SERIAL PRIMARY KEY,
     service_type_name TEXT unique not null,
@@ -118,7 +102,22 @@ CREATE TABLE myadvisor.service_types (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
+INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Retirement Planning', 'retirement_planning');
+INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Investment Management', 'investment_management');
+INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Tax Planning', 'tax_planning');
+INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Estate Planning', 'estate_planning');
+INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Insurance Planning', 'insurance_planning');
+INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Education Planning', 'education_planning');
+INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Debt Management', 'debt_management');
+INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Small Business Planning', 'small_business_planning');
+INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Divorce Planning', 'divorce_planning');
+INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Elder Care Planning', 'elder_care_planning');
+INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Charitable Giving And Philanthropy', 'charitable_giving_and_philanthropy');
+INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Behavioral Finance', 'behavioral_finance');
+INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Wealth Management', 'wealth_management');
+INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Risk Management', 'risk_management');
+INSERT INTO myadvisor.service_types (service_type_name, service_type_code) VALUES ('Financial Education And Coaching', 'financial_education_and_coaching');
+    
 CREATE TABLE myadvisor.advisor_service (
     advisor_id INT REFERENCES myadvisor.advisors(advisor_id) ON DELETE CASCADE,
     service_id INT REFERENCES myadvisor.service_types(service_id) ON DELETE CASCADE,
